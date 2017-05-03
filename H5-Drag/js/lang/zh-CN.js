82ecd3ee205ec5e89601a3f61068adf5eab6cca6
ch <caobody@gmail.com>
2017-05-04 00:18:38 +0800

    H5-Drag

---
+++ H5-Drag/js/lang/zh-CN.js
@@ -0,0 +1,104 @@
+/********************
+*@param function:e/下 javascript 简体语言包
+*@param datetime 2016-10-20
+*@param author abin
+********************/
+var EP_lang={
+  'success':'成功',
+  'error':'失败',
+  'btn_edit':'编辑',
+  'btn_copy':'复制',
+  'btn_cancel':'取消',
+  'btn_sure':'确定',
+  'btn_remove':'移除',
+  'mobile_menu':'选单',
+
+  //page list form
+  'page_title_empty_error':'请输入页面标题',
+  'page_title_maxLength_error':'页面标题不能超过175个字,请重新输入。',
+  'waterfall_ajax_end_tips':'您已经到达列表的最底！',
+  //#page_from_view#
+  'beforeunload_tips':'您输入的内容尚未保存，确定离开此页面吗？',
+  'tips_content_sync':'有未发布的更改，点击发布！',
+  'page_description_maxLength_error':'页面描述长度超过500字,请重新输入。',
+  'page_uniqueURL_maxLength_error':'自定网址长度超过20字,请重新输入。',
+  'page_uniqueURL_format_error':'自定网址只支持字母/数字/下划线/破折号',
+  'page_password_maxLength_error':'页面密码长度超过20字,请重新输入。',
+  'block_btn_install':'安装',
+  'block_btn_uninstall':'解除',
+
+  //#draganddrop.js#
+  'draganddrop-row-handle':'拖动',
+  'draganddrop-row-style':'风格',
+  'draganddrop-row-up':'上移',
+  'draganddrop-row-down':'下移',
+  'draganddrop-row-copy':'复制',
+  'draganddrop-row-remove':'移除',
+  'draganddrop-block':'方块',
+  'draganddrop-style':'风格',
+  'draganddrop-block_insert':'添加',
+  'draganddrop-block-more':'安装更多',
+
+  //#toolbar#
+  'toolbar-icon-bold':'粗体',
+  'toolbar-icon-italic':'斜体',
+  'toolbar-icon-underline':'下划线',
+  'toolbar-icon-droplet':'阴影',
+  'toolbar-icon-strike':'删除线',
+  'toolbar-icon-color':'字体颜色',
+  'toolbar-icon-fontsize':'字体大小',
+    'toolbar-fontsize-default':'默认',
+  'toolbar-icon-eraser':'清除格式',
+  'toolbar-icon-header':'标题',
+    'toolbar-headings-global':'标题',
+    'toolbar-headings-paragraph':'段落',
+  'toolbar-icon-font':'字体',
+    'toolbar-fonts-none':'无',
+  'toolbar-icon-align':'对齐',
+  'toolbar-icon-list':'列表',
+  'toolbar-icon-link':'连结',
+  'toolbar-icon-unlink':'移除连结',
+
+  //#link modal#
+  'modalLink-txtLink':'连结:',
+  'modalLink-LinkText':'文字:',
+  'modalLink-target':'目标:',
+  'modalLink-NewWindow':'新视窗',
+  'modalLink-create-tips':'请选择一些文字。',
+
+  //#text align modal#
+  'md-align-left':'居左对齐',
+  'md-align-center':'居中对齐',
+  'md-align-right':'居右对齐',
+  'md-align-justify':'左右对齐',
+
+  'md-list-indent':'缩进',
+  'md-list-outdent':'凸出',
+  'md-list-bulleted':'项目符号',
+  'md-list-numbered':'数字符号',
+
+  'md-color-textColor':'文字颜色',
+  'md-color-bgColor':'文字背景颜色',
+  'md-color-reset':'重置',
+  'md-color-custom':'自定颜色',
+  'colorpicker_lang':'主题颜色,标准颜色,网页颜色,主题颜色,返回,使用记录,无记录.',
+
+  'md-delrowconfirm-tips':'您确认移除这个方块内容？',
+
+  //#upload#
+  'fileupload-maxFileSize':'您选择的图片大小超过 5 MB',
+  'fileupload-acceptFileTypes':'对不起,您选择的文件格式不正确 (JPG,GIF,PNG)',
+
+  //platform\js\common.js #userProfile and userPassword#
+  'nickname_empty_error':'请输入您的暱称',
+  'nickname_maxLength_error':'您输入的名称不能超过50个字,请重新输入',
+  'current_password_empty_error':'请输入原密码',
+  'current_password_minLength_error':'请输入至少六位原密码',
+  'new_password_empty_error':'请输入新密码',
+  'new_password_minLength_error':'请输入至少六位新密码',
+  'confirm_password_empty_error':'请输入确认新密码',
+  'confirm_password_minLength_error':'请输入至少六位确认新密码',
+  'confirm_password_match_error':'两次密码输入不一样',
+  //setting
+  'setting_subdomain_error':'对不起,网址必须是3至15个字母或数字组合!'
+};
